da7726af9d8a5b940f06cb15d7c1c68189f44c90
Mr-andrei <91520213+Mr-andrei@users.noreply.github.com>
2021-11-20 11:02:45 +0300

    Updates

---
+++ precache-manifest.2ea691bcc2b21050129d7f82c9a0d75b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1bcfba7e21778674efa077855abd46",
+    "url": "/Mr-andrei.github.io/DZFromIgnat/index.html"
+  },
+  {
+    "revision": "5af842a08d20fdff9032",
+    "url": "/Mr-andrei.github.io/DZFromIgnat/static/css/main.76ba1a38.chunk.css"
+  },
+  {
+    "revision": "f302376ec5920fb247fb",
+    "url": "/Mr-andrei.github.io/DZFromIgnat/static/js/2.97f4e85d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Mr-andrei.github.io/DZFromIgnat/static/js/2.97f4e85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af842a08d20fdff9032",
+    "url": "/Mr-andrei.github.io/DZFromIgnat/static/js/main.eaff1905.chunk.js"
+  },
+  {
+    "revision": "e756288531fbd4ffa259",
+    "url": "/Mr-andrei.github.io/DZFromIgnat/static/js/runtime-main.68f59161.js"
+  }
+]);
